5843a955698e19d56956c8b82c507592ebb47ed1
Ruslan <rasel579@gmail.com>
2019-03-08 00:30:46 +0500

    create a new action for fetching data from firebase

--- src/actions/employeeActions.js
+++ src/actions/employeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types'; 
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_FETCH_SUCCESS } from './types'; 
 export const employeeUpdate = ({ prop, value }) => {
   return {
       type: EMPLOYEE_UPDATE,
@@ -20,4 +20,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
             Actions.pop()
         });
     };
+};
+
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+
+   return (dispatch) => {
+      firebase.database().ref(`/users/${currentUser.uid}/employees`)
+       .on('value', snapshot => {
+           dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+       });
+   };
 };
