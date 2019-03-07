b551c2c506ae1ea66433240f2bb50e6087d2b17f
Ruslan <rasel579@gmail.com>
2019-03-07 22:38:24 +0500

    add new actions for creating collections in firebase for EmployeeCreate component

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import { EMPLOYEE_UPDATE } from '../actions/types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
 import { ActionConst } from 'react-native-router-flux';
 
 const INITIAL_STATE = {
@@ -9,10 +9,13 @@ const INITIAL_STATE = {
 
 
 export default (state = INITIAL_STATE, action) => {
+    console.log(action);
     switch(action.type){
         case EMPLOYEE_UPDATE:
             // action.payload = { prop: 'name', value: 'Jane'}
-            return { ...state, [action.payload.prop]: action.payload.value }
+            return { ...state, [action.payload.prop]: action.payload.value };
+        case EMPLOYEE_CREATE:
+          return INITIAL_STATE;    
         default:
          return state;
     }
