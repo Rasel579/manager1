546229b564689c7340031a0884bb52545b9c59c7
Ruslan <rasel579@gmail.com>
2019-03-14 12:23:04 +0500

    Create a List of employees, and start to make a reuseble component EmployeeForm for showing information of each employee

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -11,6 +11,7 @@ class EmployeeCreate extends Component {
         this.props.employeeCreate({ name, phone, shift : shift || 'Monday'});
     }
     render(){
+        console.log(this.props.employee);
         return(
             <Card>
                 <CardSection>
--- src/components/EmpolyeeList.js
+++ src/components/EmpolyeeList.js
@@ -1,41 +1,34 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, ListView } from 'react-native';
-import { employeeFetch } from '../actions';
+import { FlatList, View, Text } from 'react-native';
+import { employeesFetch } from '../actions/';
+import  ListItem  from './ListItem';
+
 
 
 class EmployeeList extends Component {
-      componentWillMount(){
-         this.props.employeeFetch();
-         
-        this.createDataSource(this.props);
-      };
-
-      componentWillReceiveProps(nextProps){
-         // nextProps are the next set of props that this component
-         // will be rendered with
-         // this.props is still old set of props
-         this.createDataSource(nextProps);
-      };
-
-      createDataSource({ employees}){
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
-
-        this.dataSource = ds.cloneWithRows(employees);
-      };
-    render(){
-        return (
-              <View>
-                  <Text>Employee List</Text>
-                  <Text>Employee List</Text>
-                  <Text>Employee List</Text>
-                  <Text>Employee List</Text>
-                  <Text>Employee List</Text>
-              </View>
-            );
+  componentDidMount() {
+    this.props.employeesFetch();
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.employees.length !== this.props.employees.length) {
+      this.props.employeesFetch();
     }
+  }
+    render(){
+      return (
+        <FlatList data={this.props.employees} renderItem={({item}) => <ListItem employee={item}/>}/>
+      );
+    };
 }
 
-export default connsect(null, { employeeFetch})(EmployeeList);
+
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid}
+  });
+  return { employees };
+}
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
